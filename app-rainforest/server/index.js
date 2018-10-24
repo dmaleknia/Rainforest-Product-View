bc5ef00fdf44219a9fbbb2fea8d5f9b7736412cd
Dominic M <dominicccc@gmail.com>
2018-10-24 15:44:38 -0700

    added more middleware

--- app-rainforest/server/index.js
+++ app-rainforest/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+
 const axios = require('axios');
 const _ = require('lodash');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
+app.use(morgan);
 
 
 const port = 710;
