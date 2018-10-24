9c142add37e9c0bc9b444ebc1fa78bbc967fb402
Dominic M <dominicccc@gmail.com>
2018-10-24 16:07:54 -0700

    working on frontend bugs

--- app-rainforest/react-client/src/index.jsx
+++ app-rainforest/react-client/src/index.jsx
@@ -10,58 +10,9 @@ class App extends React.Component {
       blogs: []
     }
 
-    this.changeView = this.changeView.bind(this);
   }
 
-  componentDidMount() {
-    this.getBlogs();
-  }
 
-  getBlogs() {
-    axios.get('http://127.0.0.1:3000/api/blogs')
-      .then((response) => {
-        this.setState({
-          blogs: response.data
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  changeView(option) {
-    if (option === 'feed') {
-      this.setState({
-        view: 'feed'
-      });
-    } else if (option === 'admin') {
-      this.setState({
-        view: 'admin'
-      });
-    } else if (option === 'create') {
-      this.setState({
-        view: 'create'
-      });
-    } else {
-      this.setState({
-        view: this.state.blogs[option]
-      });
-    }
-  }
-
-  renderView() {
-    const { view } = this.state;
-
-    if (view === 'feed') {
-      return <Feed blogs={this.state.blogs} handleClick={(e) => this.changeView(e)} />
-    } else if (view === 'admin') {
-      return <Admin blogs={this.state.blogs} />
-    } else if (view === 'create') {
-      return <Create />
-    } else {
-      return <Post blog={this.state.view} />
-    }
-  }
   render() {
     return (
       <div>
