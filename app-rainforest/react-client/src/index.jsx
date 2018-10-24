8f60ba440c7c2f5d7fbadbc66e0a5789329f637e
Dominic M <dominicccc@gmail.com>
2018-10-24 16:05:50 -0700

    working on frontend issues

--- app-rainforest/react-client/src/index.jsx
+++ app-rainforest/react-client/src/index.jsx
@@ -2,14 +2,74 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      view: 'feed',
+      blogs: []
+    }
+
+    this.changeView = this.changeView.bind(this);
+  }
+
+  componentDidMount() {
+    this.getBlogs();
+  }
+
+  getBlogs() {
+    axios.get('http://127.0.0.1:3000/api/blogs')
+      .then((response) => {
+        this.setState({
+          blogs: response.data
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  changeView(option) {
+    if (option === 'feed') {
+      this.setState({
+        view: 'feed'
+      });
+    } else if (option === 'admin') {
+      this.setState({
+        view: 'admin'
+      });
+    } else if (option === 'create') {
+      this.setState({
+        view: 'create'
+      });
+    } else {
+      this.setState({
+        view: this.state.blogs[option]
+      });
+    }
+  }
+
+  renderView() {
+    const { view } = this.state;
+
+    if (view === 'feed') {
+      return <Feed blogs={this.state.blogs} handleClick={(e) => this.changeView(e)} />
+    } else if (view === 'admin') {
+      return <Admin blogs={this.state.blogs} />
+    } else if (view === 'create') {
+      return <Create />
+    } else {
+      return <Post blog={this.state.view} />
+    }
   }
   render() {
-    return (  );
+    return (
+      <div>
+        <span>Hello from app!</span>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app');
+const app = document.getElementById('app');
+ReactDOM.render(<App />, app);
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
         test: /\.jsx?/,
         include: SRC_DIR,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        options: {
+          presets: ['@babel/env']
         }
       }
     ]
