f844d7b51c6ece7570135eddace4653ab6cbeb36
Dominic M <dominicccc@gmail.com>
2018-10-24 19:33:06 -0700

    added Display

---
+++ app-rainforest/react-client/src/components/display.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (  );
+  }
+}
+
+export default Display;
