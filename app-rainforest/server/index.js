cd92e5169163b848fc08c29ba39d00b4c59323bb
Dominic M <dominicccc@gmail.com>
2018-10-29 13:35:25 -0700

    Reviewing files

--- app-rainforest/server/index.js
+++ app-rainforest/server/index.js
@@ -18,4 +18,4 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
