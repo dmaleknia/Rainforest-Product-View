ae1dbdbb2b5e1a7fc2929cb4e2da9490819afead
Dominic M <dominicccc@gmail.com>
2018-10-29 15:38:57 -0700

    (init)

---
+++ app-rainforest/react-client/src/components/Display.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Photo from './Photo.jsx';
+import VideoPlayer from './VideoPlayer.jsx';
+
+class Display extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+  }
+  render() {
+    if (this.props.displayed) {
+      return (
+        <span className='Display'>Display
+          <iframe width='420' height='315' src={this.props.image}></iframe>
+        </span>
+      );
+    } else {
+      return (
+        <span></span>
+      );
+    }
+  }
+}
+
+export default Display;
