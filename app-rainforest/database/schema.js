a7aa7d2c3fb0d879c4953958436fb179f6cd7faa
Dominic M <dominicccc@gmail.com>
2018-10-30 20:19:48 -0700

    added comment

--- app-rainforest/database/schema.js
+++ app-rainforest/database/schema.js
@@ -29,3 +29,21 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
 }
+
+// module.exports = (sequelize, DataTypes) => {
+//   return sequelize.define('product', {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       unique: true
+//     },
+//     name: DataTypes.STRING,
+//     image_1_url: DataTypes.STRING,
+//     image_2_url: DataTypes.STRING,
+//     image_3_url: DataTypes.STRING,
+//     image_4_url: DataTypes.STRING,
+//     image_5_url: DataTypes.STRING,
+//     image_6_url: DataTypes.STRING,
+//     video_url: DataTypes.STRING,
+//   });
+// }
