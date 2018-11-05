a0b0f0ee924268f9fe8862d40126a3585695505a
Dominic M <dominicccc@gmail.com>
2018-11-04 19:29:59 -0800

    added some video links

---
+++ app-rainforest/database/videos.js
@@ -0,0 +1,12 @@
+exports.videos = [
+  'https://www.youtube.com/embed/BoDQEuP8Igo',
+  'https://www.youtube.com/embed/PtQ0V734zxU',
+  'https://www.youtube.com/embed/eZDwirwl_7U',
+  'https://www.youtube.com/embed/xpYqvrYy-xw',
+  'https://www.youtube.com/embed/PPFNaMvnMtM',
+  'https://www.youtube.com/embed/4Y1v9heACD8',
+  'https://www.youtube.com/embed/nESGCanIwps',
+  'https://www.youtube.com/embed/54jaHtBkpjA',
+  'https://www.youtube.com/embed/2_k7i8m0Tjc',
+  'https://www.youtube.com/embed/-HoNSAmxpFc'
+];
