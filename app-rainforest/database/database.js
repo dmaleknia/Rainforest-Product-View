9c49a7087e1ab2ab62d930186f4c9435c030ddf0
Dominic M <dominicccc@gmail.com>
2018-11-05 18:14:11 -0800

    working on bug

--- app-rainforest/database/database.js
+++ app-rainforest/database/database.js
@@ -2,11 +2,6 @@ const Sequelize = require('sequelize');
 const images = require('./images.js').images;
 const videos = require('./videos.js').videos;
 
-// const sequelize = new Sequelize('product_view', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql'
-// });
-
 const getImage = () => {
   let index = Math.floor((Math.random() * (images.length - 1)));
   return images[index];
@@ -25,6 +20,7 @@ const seed = () => {
   sequelize.query(
     'CREATE TABLE products (id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), image_1_url VARCHAR(255), image_2_url VARCHAR(255), image_3_url VARCHAR(255), image_4_url VARCHAR(255), image_5_url VARCHAR(255), image_6_url VARCHAR(255), video_url VARCHAR(255), category VARCHAR(255));')
     .then(data => {
+      console.log(data);
       for (let i = 0; i < 100; i++) {
         let name = `product ${i}`;
         let image_1_url = getImage();
@@ -36,9 +32,6 @@ const seed = () => {
         let video_url = getVideo();
         let category = `category ${i}`;
 
-        console.log(image_6_url);
-        console.log(video_url);
-
         sequelize.query(
           `INSERT INTO products (id, name, image_1_url, image_2_url, image_3_url, image_4_url, image_5_url, image_6_url, video_url, category) VALUES ('${i}', '${name}', '${image_1_url}', '${image_2_url}', '${image_3_url}', '${image_4_url}', '${image_5_url}', '${image_6_url}', '${video_url}', '${category}')`, {
             type: sequelize.QueryTypes.INSERT
@@ -46,31 +39,10 @@ const seed = () => {
         );
       }
       return data;
+    })
+    .catch(err => {
+      console.log(err.original);
     });
 };
 
 seed();
-
-// sequelize.query("CREATE DATABASE product_view;")
-//   .then(() => {
-//     // sequelize.close();
-//     // sequelize = new Sequelize('product_view', 'root', '', {
-//     //   host: 'localhost',
-//     //   dialect: 'mysql'
-//     // });
-//     seed();
-//   })
-//   .catch(err => {
-//     console.log(err);
-
-//     // sequelize.query("CREATE DATABASE product_view;")
-//     //   .then(() => {
-//     //     sequelize.close();
-//     //     sequelize = new Sequelize('product_view', 'root', '', {
-//     //       host: 'localhost',
-//     //       dialect: 'mysql'
-//     //     });
-//     //     seed();
-//     //   })
-
-//   });
