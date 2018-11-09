9478bf82a445760a36fd4dbd51ca20caa70873f8
Dominic M <dominicccc@gmail.com>
2018-11-08 17:35:53 -0800

    added knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.CR_URL,
+    // connection: 'postgres://localhost/rainforest',
+    migrations: {
+      directory: __dirname + '/database/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds/development'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: process.env.CR_URL,
+    // connection: {
+    //   database: 'rainforest',
+    //   user:     'rain',
+    //   password: 'forest'
+    // },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.CR_URL,
+    // {
+    //   database: 'rainforest',
+    //   user:     'rain',
+    //   password: 'forest'
+    // },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
