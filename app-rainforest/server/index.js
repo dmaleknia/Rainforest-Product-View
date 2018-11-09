9eca808f17c50660913eced4b5522167182eb832
Dominic M <dominicccc@gmail.com>
2018-11-08 19:23:47 -0800

    removed comment

--- app-rainforest/server/index.js
+++ app-rainforest/server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const Sequelize = require('sequelize');
 
-const environment = process.env.NODE_ENV || 'development'; // look at!
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require(__dirname + '/../../knexfile.js')[environment];
 const database = require('knex')(configuration);
 
